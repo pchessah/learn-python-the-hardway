935e9d2a3567f035f61f471b4ead72631bfd4092
Paul Chesa <paulchesa1@gmail.com>
2021-03-02 21:09:07 +0300

    exercise 6

--- ex3.py
+++ ex3.py
@@ -1,8 +1,22 @@
+#Prints
 print("I will now count my chickens: ")
 
-print("hens", 25 +30 /6)
+print("hens", 25 +30 /6.0)
 print("Roosters", 100 -25 *3%4)
 
 print("Now I will count the eggs: ")
-print(3+2+1-5+4%2-1/4+6)
+print(3+2+1-5+4%2-1/4+6.0)
 
+
+print("Is it true that 3+2 < 5 -7?")
+print(3+2<5-7.0)
+
+print("what is 3+2?", 3+2.0)
+print("what is 5 -7", 5-7.0)
+
+print("That is why it is false")
+print("How about some more.")
+
+print("Is it greater?", 5 > -2.0)
+print("Is it greater or equal", 5>=-2.0)
+print("Is it less or equal", 5<=-2)
---
+++ ex5.py
@@ -0,0 +1,16 @@
+name = "Zed A. Shaw"
+age = 35
+height =  74
+weight = 180
+eyes = "blue"
+teeth = "white"
+hair = "brown"
+
+print(f"Let's talk about{name}")
+print(f"He is  {height * 2.54} cm tall")
+print(f"He is {weight * round(0.45359)} kgs heavy")
+print(f"He has {eyes} eyes and  {hair} hair")
+print(f"His teeth are usually {teeth} depending on the coffee")
+
+total =  age + height + weight
+print(f"If i added {age}, {height} and {weight} I get {total}")
