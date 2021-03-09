029857a4ceeeddd98d6e689d2789b01440c8b789
Paul Chesa <paulchesa1@gmail.com>
2021-03-09 16:32:59 +0300

    boolean expressions

---
+++ ex27.py
@@ -0,0 +1,35 @@
+# BOOLEAN EXPRESSIONS
+
+#  True and True ==> True
+#  True and False ==> False
+#  False and False ==> False
+#  False and True ==> False
+
+# True or True ==> True
+# True or False ==> True
+# False or False => False
+# False or True => True
+
+
+# Not(True and True) ==> False
+# Not(True and False) => True
+# Not(False and False) => True
+# Not(False and True) => True
+
+
+# Not(True or False) ===> False
+# Not(True or True) ===> False
+# Not(False or False) ===> True
+# Not (false or true) ===> False
+
+
+# 0 == 1 ===> False
+# 0 != 1 ====> True
+# 0 == 0 ===> True
+
+# 1 == 0 ==> False
+# 1 != 0 ==> True
+# 1 == 1 ==> True
+
+
+
